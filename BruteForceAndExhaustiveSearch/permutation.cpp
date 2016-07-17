371b94d191b163785b997ed98af4a9352f427c99
Naomi <naominewton@163.com>
2016-07-17 16:20:57 +0800

    first commit on July 17,2016

---
+++ BruteForceAndExhaustiveSearch/permutation.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	
+ } 
+ 
+ void print_permutation(int n,int* A,int cur)
+ {
+ 	if(cur==n)
+ 	{
+ 		for(int i=0;i<cur;i++)//output A
+ 		  printf("%d ",A[i]);
+ 		printf("\n");  
+	 }
+	else
+	{
+		for(int i=)
+	 } 
+ }
---
+++ BruteForceAndExhaustiveSearch/test.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+
+  int a[2][2]={-1};
+  for(int i=0;i<2;i++)
+    for(int j=0;j<2;j++)
+	  printf("%d \n",a[i][j]);  
+  return 0;  
+}
